179d4cedfa0e8c35c2e039374231a5f8d09f5f6b
maykongpedro <maykonglaffite@gmail.com>
2021-07-21 19:30:34 -0300

    adicionar documentação mapeamento estadual

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Base de dados contendo os mapeamentos de plantios florestais no Brasil,
 #' a nivel de municipio.
 #'
-#' @format Uma tibble com 32,588 linhas e 12 colunas, contendo:
+#' @format Uma tibble com 32,588 linhas e 10 colunas, contendo:
 #' \describe{
 #'   \item{mapeamento}{Nome do mapeamento ou relatorio de origem}
 #'   \item{fonte}{Fonte dos dados, nao e necessariamente o responsavel pelo
@@ -18,6 +18,33 @@
 #'   \item{area_ha}{Area plantada em hectares}
 #' }
 #' @name mapeamentos_municipios
-#' @source Para detalhes, veja o seguinte repositorio: - \url{https://github.com/maykongpedro/2021-07-04-extracao-mapeamentos-plantios-florestais}
+#' @source Os relatorios e mapeamentos utilizados constam no README do pacote.
+#' Para detalhes, veja o seguinte repositorio: - \url{https://github.com/maykongpedro/2021-07-04-extracao-mapeamentos-plantios-florestais}
 #' @examples head(mapeamentos_municipios)
 "mapeamentos_municipios"
+
+#' Dados sobre os mapeamentos em nivel estadual de plantios florestais no Brasil
+#'
+#' Base de dados contendo os mapeamentos de plantios florestais no Brasil,
+#' a nivel de estado (uniao federativa).
+#'
+#' @format Uma tibble com 976 linhas e 10 colunas, contendo:
+#' \describe{
+#'   \item{mapeamento}{Nome do mapeamento ou relatorio de origem}
+#'   \item{fonte}{Fonte dos dados, nao e necessariamente o responsavel pelo
+#'   relatorio}
+#'   \item{ano_base}{Ano base do mapeamento, difere do ano em que o relatorio foi
+#'   publicado, geralmente}
+#'   \item{uf}{Unidade Federativa da Uniao}
+#'   \item{estado}{Nome do Estado da Uniao}
+#'   \item{regiao}{Macro regiao do estado, utilizado no Parana e Santa Catarina}
+#'   \item{nucleo_regional}{Nucleo regional do estado, utilizado no Parana}
+#'   \item{corede}{Corede do estado, utilizado no Rio Grande do Sul}
+#'   \item{genero}{Genero do plantio florestal}
+#'   \item{area_ha}{Area plantada em hectares}
+#' }
+#' @name mapeamentos_estados
+#' @source Os relatorios e mapeamentos utilizados constam no README do pacote.
+#' Para detalhes, veja o seguinte repositorio: - \url{https://github.com/maykongpedro/2021-07-04-extracao-mapeamentos-plantios-florestais}
+#' @examples head(mapeamentos_estados)
+"mapeamentos_estados"
