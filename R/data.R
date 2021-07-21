15e025f10e2b8b3563aa6f859a03daac97295100
maykongpedro <maykonglaffite@gmail.com>
2021-07-20 23:55:21 -0300

    retirando acentos

--- R/data.R
+++ R/data.R
@@ -1,23 +1,23 @@
-#' Dados sobre os mapeamentos em nível municipal de plantios florestais no Brasil
+#' Dados sobre os mapeamentos em nivel municipal de plantios florestais no Brasil
 #'
 #' Base de dados contendo os mapeamentos de plantios florestais no Brasil,
-#' a nível de município.
+#' a nivel de municipio.
 #'
 #' @format Uma tibble com 32,588 linhas e 12 colunas, contendo:
 #' \describe{
-#'   \item{mapeamento}{Nome do mapeamento ou relatório de origem}
-#'   \item{fonte}{Fonte dos dados, não é necessariamente o responsável pelo
-#'   relatório}
-#'   \item{ano_base}{Ano base do mapeamento, diferere do ano que o relatório foi
+#'   \item{mapeamento}{Nome do mapeamento ou relatorio de origem}
+#'   \item{fonte}{Fonte dos dados, nao e necessariamente o responsavel pelo
+#'   relatorio}
+#'   \item{ano_base}{Ano base do mapeamento, difere do ano em que o relatorio foi
 #'   publicado, geralmente}
-#'   \item{uf}{Unidade Federativa da União}
-#'   \item{estado}{Nome do Estado da União}
-#'   \item{municipio}{Nome do município}
-#'   \item{code_muni}{Código único do município pelo IBGE}
-#'   \item{genero}{Gênero do plantio florestal}
-#'   \item{area_ha}{Área plantada (ha)}
+#'   \item{uf}{Unidade Federativa da Uniao}
+#'   \item{estado}{Nome do Estado da Uniao}
+#'   \item{municipio}{Nome do municipio}
+#'   \item{code_muni}{Codigo unico do municipio pelo IBGE}
+#'   \item{genero}{Genero do plantio florestal}
+#'   \item{area_ha}{Area plantada em hectares}
 #' }
 #' @name mapeamentos_municipios
-#' @source Diversos - \url{https://github.com/maykongpedro/2021-07-04-extracao-mapeamentos-plantios-florestais}
+#' @source Para detalhes, veja o seguinte repositorio: - \url{https://github.com/maykongpedro/2021-07-04-extracao-mapeamentos-plantios-florestais}
 #' @examples head(mapeamentos_municipios)
 "mapeamentos_municipios"
