d0cf9d7c313ed04d77482d2b0f6b7210e5ac089c
Maykon G Pedro <maykonglaffite@gmail.com>
2021-07-25 21:20:42 -0300

    add função de exportar csv

--- R/exportar_dados.R
+++ R/exportar_dados.R
@@ -24,7 +24,7 @@ exportar_xlsx <- function(caminho_para_salvar_bases, ...){
 
     # Nome padrão do arquivo
     nome_arquivo <- paste0(caminho_para_salvar_bases,
-                             "/mapeamentos_plantios_florestais",
+                             "mapeamentos_plantios_florestais",
                              ".xlsx")
 
     # Exportar base
@@ -33,5 +33,38 @@ exportar_xlsx <- function(caminho_para_salvar_bases, ...){
 
 }
 
+#' Exportar bases em formato .csv separado por ';' e com enconding UTF-8.
+#'
+#' @param caminho_para_salvar_bases caminho da pasta para salvar o arquivo, como
+#' "C:/documentos/" ou "./" para savar no projeto ativo
+#' @param ... parametros adicionais para a exportacao do csv
+#'
+#' @return invisivel
+#' @export
+#'
+#' @examples
+exportar_csv <- function(caminho_para_salvar_bases, ...){
 
+
+    # Bases
+    map_muni <- plantiosflorestais::mapeamentos_municipios
+    map_uf <- plantiosflorestais::mapeamentos_estados
+
+    # Gerar nome arquivo de municípios
+    caminho_base_muni <- paste0(caminho_para_salvar_bases,
+                                "mapeamentos_municipios",
+                                ".csv")
+
+
+    # Gerar nome arquivo de estados
+    caminho_base_estados <- paste0(caminho_para_salvar_bases,
+                                   "mapeamentos_estados",
+                                   ".csv")
+
+
+    # Exportar bases
+    readr::write_csv2(map_muni, file = caminho_base_muni, ...)
+    readr::write_csv2(map_uf, file = caminho_base_estados, ...)
+
+}
 
